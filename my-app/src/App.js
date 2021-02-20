ea1b160cccc83061c2df7f34c69f6f8a91aeaaf9
iam-Mai <sasithorn.senmaithai@gmail.com>
2021-02-20 11:02:10 +0700

    init firebase config

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,28 @@
 import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+      apiKey: "AIzaSyBT6kuWKZTRm-oXmnbQQWWkmByMqNjOtAk",
+      authDomain: "chatapp-c3a63.firebaseapp.com",
+      projectId: "chatapp-c3a63",
+      storageBucket: "chatapp-c3a63.appspot.com",
+      messagingSenderId: "1098887902648",
+      appId: "1:1098887902648:web:342320222c6d273f078b4d",
+      measurementId: "G-5ZVMCX0P57"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
 function App() {
   return (
